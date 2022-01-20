01f39f567bc2e19cfb62ee70b510815801555b86
johnlecher <johnlecher117@gmail.com>
2022-01-20 17:14:52 -0600

    removed unneeded subsystems

--- src/Robot.h
+++ src/Robot.h
@@ -6,17 +6,8 @@
 #include <frc/TimedRobot.h>
 #include <CORERobotLib.h>
 #include <COREFramework/COREScheduler.h>
-#include "ClimberSubsystem.h"
-#include "ConveyorSubsystem.h"
-#include "CameraSubsystem.h"
 #include "DriveSubsystem.h"
-#include "IntakeSubsystem.h"
-#include "LauncherSubsystem.h"
-#include "TurretSubsystem.h"
 #include "Config.h"
-#include "auto/TestRoutine.h"
-#include "auto/DriveRoutine.h"
-#include "auto/ThreeBallRoutine.h"
 
 using namespace CORE;
 using namespace std;
@@ -31,15 +22,6 @@ public:
 	void testInit() override;
 	static Robot * GetInstance();
 	DriveSubsystem driveSubsystem;
-	LauncherSubsystem launcherSubsystem;
-	IntakeSubsystem intakeSubsystem;
-	TurretSubsystem turretSubsystem;
-	ConveyorSubsystem conveyorSubsystem;
-	ClimberSubsystem climberSubsystem;
-	CameraSubsystem cameraSubsystem;
-	TestRoutine testRoutine;
-	DriveRoutine driveRoutine;
-	ThreeBallRoutine threeBallRoutine;
 
 private:
 	static Robot * m_instance;
