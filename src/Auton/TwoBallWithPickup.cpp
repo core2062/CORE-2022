ed26b8a1adb642f687b5838fd38038571ca4a641
CORE Programmers <core2062Programmers@googlegroups.com>
2022-03-12 14:41:35 -0600

    Updated Two Ball With Pickup

--- src/Auton/TwoBallWithPickup.cpp
+++ src/Auton/TwoBallWithPickup.cpp
@@ -14,11 +14,13 @@ void TwoBallWithPickup::AddNodes() {
     delayNode2 = new Node(1, new DelayAction()); // TODO: Figure out length of delay
     delayNode3 = new Node(1, new DelayAction()); // TODO: Figure out length of delay
     delayNode4 = new Node(1, new DelayAction()); // TODO: Figure out length of delay
+    delayNode5 = new Node(1, new DelayAction()); // TODO: Figure out length of delay
     conveyorNode1 = new Node(5, new ConveyorAction(CONVEYOR_ON));
     conveyorNode2 = new Node(5, new ConveyorAction(CONVEYOR_OFF));
     launcherNode1 = new Node(5, new LauncherAction(LAUNCHER_FORWARD));
     launcherNode2 = new Node(5, new LauncherAction(LAUNCHER_OFF));
-    
+    launcherPunchNode1 = new Node(5, new LauncherAction(LAUNCHER_PUNCH_IN));
+    launcherPunchNode2 = new Node(5, new LauncherAction(LAUNCHER_PUNCH_OUT));
     AddFirstNode(driveNode1);
     driveNode1->AddNext(extendNode);
     extendNode->AddNext(intakeNode1);
@@ -26,12 +28,15 @@ void TwoBallWithPickup::AddNodes() {
     delayNode1->AddNext(conveyorNode1);
     conveyorNode1->AddNext(delayNode2);
     delayNode2->AddNext(driveNode2);
-    driveNode2->AddNext(launcherNode1);
+    driveNode2->AddNext(launcherPunchNode1);
+    launcherPunchNode1->AddNext(launcherNode1);
     launcherNode1->AddNext(delayNode3);
-    delayNode3->AddNext(launcherNode2);
-    launcherNode1->AddNext(driveNode3);
+    delayNode3->AddNext(launcherPunchNode2);
+    launcherPunchNode2->AddNext(launcherNode2);
+    launcherNode2->AddNext(delayNode4);
+    delayNode4->AddNext(driveNode3);
     driveNode3->AddNext(driveNode4);
-    driveNode4->AddNext(delayNode4);
-    delayNode4->AddNext(intakeNode2);
+    driveNode4->AddNext(delayNode5);
+    delayNode5->AddNext(intakeNode2);
     intakeNode2->AddNext(conveyorNode2);
 }
