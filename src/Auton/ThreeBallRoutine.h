0af4a97b946ee77fb756482e88770955257955d5
William Markwyn <wmarkwyn@gmail.com>
2022-03-12 14:43:08 -0600

    Updated Three Ball

--- src/Auton/ThreeBallRoutine.h
+++ src/Auton/ThreeBallRoutine.h
@@ -23,14 +23,21 @@ class ThreeBallRoutine : public COREAuton {
     Node * conveyorNode1;   // Conveyor on to make space for second ball
     Node * delayNode2;      // Delay to intake ball
     Node * driveNode2;      // Turning robot to aim
+    Node * launcherPunchInNode1; 
     Node * launcherNode1;   // Turning on launcher
     Node * delayNode3;      // Delay to launch all balls
+    Node * launcherPunchOutNode1; 
+    Node * launcherPunchInNode2; 
     Node * launcherNode2;   // Turns launcher off
+    Node * delayNode4;
+    Node * launcherPunchOutNode2;
     Node * driveNode3;      // Turns robot to aim to next ball
     Node * driveNode4;      // Drives to next ball
     Node * driveNode5;      // Turns robot to aim
+    Node * launcherPunchInNode3; 
     Node * launcherNode3;   // Turns launcher on 
-    Node * delayNode4;      // Delay to launch all balls
+    Node * delayNode5;      // Delay to launch all balls
+    Node * launcherPunchOutNode3; 
     Node * intakeNode2;     // Turns intake off
     Node * conveyorNode2;   // Turns conveyor off
     Node * extendNode;      // Extend the Intake
