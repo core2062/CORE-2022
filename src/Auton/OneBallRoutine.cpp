6f0fbef5244a7b574f1c92b06f223674c2c1d23d
johnlecher <johnlecher117@gmail.com>
2022-03-24 18:26:10 -0500

    Updated One Ball

--- src/Auton/OneBallRoutine.cpp
+++ src/Auton/OneBallRoutine.cpp
@@ -5,17 +5,16 @@ OneBallRoutine::OneBallRoutine() : COREAuton("One Ball Routine") {}
 
 void OneBallRoutine::AddNodes() {
     delayNode = new Node(5, new DelayAction());
-    driveNode = new Node(5,new DriveAction(BACKWARD));
+    driveNode = new Node(5,new DriveAction(BACKWARD, 75000));
     launcherOnNode = new Node (5, new LauncherAction(LAUNCHER_FORWARD));
     launcherPunchNode = new Node (5, new LauncherAction(LAUNCH));
-    AddFirstNode(driveNode);
+    AddFirstNode(launcherOnNode);
     // CORELog::LogInfo("delay complete");
     // delayNode->AddNext(driveNode);
-    CORELog::LogInfo("drove Backward");
-    driveNode->AddNext(launcherOnNode);
-    CORELog::LogInfo("PUNCH TRIGGERED");
-    launcherOnNode->AddNext(launcherPunchNode);
-    CORELog::LogInfo("Launched");
+    CORELog::LogInfo("Activated Launcher");
+    launcherOnNode->AddNext(driveNode);
+    CORELog::LogInfo("Drove Backwards");
+    driveNode->AddNext(launcherPunchNode);
     launcherPunchNode->AddNext(delayNode);
-    CORELog::LogInfo("Delayed Again");
+    CORELog::LogInfo("Drove Again");
 }
