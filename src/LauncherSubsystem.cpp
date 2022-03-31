77a1ed8a1eab2806b77042feb049b2e42d38d689
Andrew Hunter <ahunter4213@gmail.com>
2022-03-31 18:33:58 -0500

    Updated Launcher Subsystem, punch and feed are on different timers

--- src/LauncherSubsystem.cpp
+++ src/LauncherSubsystem.cpp
@@ -2,10 +2,11 @@
 
 using namespace CORE;
 
-LauncherSubsystem::LauncherSubsystem() :    launcherForwardSpeed("Launcher Forward Speed", 0.7),
+LauncherSubsystem::LauncherSubsystem() :    launcherForwardSpeed("Launcher Forward Speed", 0.2),
                                             launcherLowSpeed("Launcher Low Speed", 0.2),
                                             launcherBackwardSpeed("Launcher Backward Speed", -0.2),
-                                            launcherDelayTime("Launcher Delay", 1.25),
+                                            launcherFeedDelayTime("Launcher Feed Delay", 1.5),
+                                            launcherPunchDelayTime("Launcher Punch Delay", 1.25),
                                             m_rightFeed(RIGHT_FEED),
                                             m_leftFeed(LEFT_FEED),
                                             m_rightLauncher(RIGHT_LAUNCHER),
@@ -19,15 +20,16 @@ void LauncherSubsystem::robotInit(){
     operatorJoystick->RegisterButton(CORE::COREJoystick::B_BUTTON);
     operatorJoystick->RegisterButton(CORE::COREJoystick::Y_BUTTON);
     operatorJoystick->RegisterButton(CORE::COREJoystick::LEFT_BUTTON);
-    m_leftFeed.Set(ControlMode::PercentOutput, 0);
-    m_rightFeed.Set(ControlMode::PercentOutput, 0);
-    m_leftLauncher.Set(ControlMode::PercentOutput, 0);
-    m_rightLauncher.Set(ControlMode::PercentOutput, 0);
+    
 }
 
 void LauncherSubsystem::teleopInit() {
     // SmartDashboard::PutString("Launcher Controls", " Launcher Out: Left Trigger \n Launcher Reverse: Left Button");
     m_launcherSolenoid.Set(DoubleSolenoid::Value::kForward);
+    m_leftLauncher.Set(ControlMode::PercentOutput, 0);
+    m_rightLauncher.Set(ControlMode::PercentOutput, 0);
+    m_leftFeed.Set(ControlMode::PercentOutput, 0);
+    m_rightFeed.Set(ControlMode::PercentOutput, 0);
     m_leftLauncher.Set(ControlMode::PercentOutput, 0);
     m_rightLauncher.Set(ControlMode::PercentOutput, 0);
 }
@@ -78,25 +80,49 @@ void LauncherSubsystem::extendLauncher(bool launcherRetracted){
 
 void LauncherSubsystem::launchCargo(){
     if (!m_launcherDelaying && m_launching) {
+            CORELog::LogInfo("Stage One Launch");
             setFeedSpeed(launcherLowSpeed.Get());
-            StartTimer();
+            StartPunchTimer();
             extendLauncher(true);
             m_launcherDelaying = true;
     } else if (m_launcherDelaying) {
-        if (GetTime() >= launcherDelayTime.Get()) {
-                setFeedSpeed(0);
-                extendLauncher(false);
-                m_launcherDelaying = false;
-                m_launching = !m_launching;
+        if (GetPunchTime() >= launcherPunchDelayTime.Get()) {
+            CORELog::LogInfo("Stage Two Launch");
+            extendLauncher(false);
+            StartFeedTimer();
+            ResetPunchTimer();
+        } else if (GetFeedTime() >= launcherFeedDelayTime.Get()) {
+            CORELog::LogInfo("Stage Three Launch");
+            setFeedSpeed(0);
+            ResetFeedTimer();
+            m_launcherDelaying = false;
+            m_launching = false;
         }
     }
 }
 
-void LauncherSubsystem::StartTimer() {
-    m_delayTimer.Reset();
-    m_delayTimer.Start();
+void LauncherSubsystem::StartPunchTimer() {
+    m_punchDelayTimer.Reset();
+    m_punchDelayTimer.Start();
 }
 
-double LauncherSubsystem::GetTime() {
-    return m_delayTimer.Get(); 
+void LauncherSubsystem::ResetPunchTimer(){
+    m_punchDelayTimer.Reset();
+}
+
+double LauncherSubsystem::GetPunchTime() {
+    return m_punchDelayTimer.Get(); 
+}
+
+void LauncherSubsystem::StartFeedTimer() {
+    m_feedDelayTimer.Reset();
+    m_feedDelayTimer.Start();
+}
+
+void LauncherSubsystem::ResetFeedTimer(){
+    m_feedDelayTimer.Reset();
+}
+
+double LauncherSubsystem::GetFeedTime() {
+    return  m_feedDelayTimer.Get();
 }
